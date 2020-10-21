d68e0bb125b843b23f391710fcef779284d09242
Dumster11 <71654055+Dumster11@users.noreply.github.com>
2020-10-21 12:28:23 +0300

    ss

---
+++ number2/number2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace number2
+public class Rectangle
+{
+    double side1, side2;
+
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    public double AreaCalculator()
+    {
+    
+    }
+
+    public double PerimeterCalculator()
+    {
+    
+    }
+
+    public double Area
+    {
+        get { return AreaCalculator(); }
+    }
+
+    public double Perimeter
+    {
+        get { return PerimeterCalculator(); }
+    }
+}
+
+public Program
+{
+    static void Main(string[] args)
+{
+    var rectangle = new Rectangle(45.55, 66.54);
+    Console.WriteLine("Area: {0}, Perimeter: {1}", rectangle.Area, rectangle.Perimeter);
+    Console.ReadLine();
+}
+}
